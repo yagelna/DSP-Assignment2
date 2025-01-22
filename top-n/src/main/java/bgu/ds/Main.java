0f934bc3d7a432f127363a7ed4b521a794d4d6cb
Yagel Nahshon <yagelnahshon@gmail.com>
2025-01-22 15:42:13 +0200

    Fix bug with order args

--- top-n/src/main/java/bgu/ds/Main.java
+++ top-n/src/main/java/bgu/ds/Main.java
@@ -7,8 +7,8 @@ public class Main {
         TopN wordCount = new TopN();
         Path input = new Path(args[0]);
         Path output = new Path(args[1]);
-        long maxSplitSize = Long.parseLong(args[3]);
-        int number = Integer.parseInt(args[2]);
+        long maxSplitSize = Long.parseLong(args[2]);
+        int number = Integer.parseInt(args[3]);
         try {
             wordCount.start(input, output, maxSplitSize, number);
         } catch (Exception e) {
